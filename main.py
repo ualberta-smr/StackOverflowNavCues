814780ae0173fe66670692f1072eeb16127ebdbd
Sarah Nadi <nadi@ualberta.ca>
2018-10-05 11:04:47 -0600

    Use enumerate for indexing list

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def find_cond_sentences(questions):
 	for question in questions['items']:
 		for answer in question['answers']:
 			paragraphs = get_paragraphs(answer['body'])
-			for paragraph_index in len(paragraphs):
-				cond_sentences = get_cond_sentences(parargaphs.get(paragraph_index), question_id=question['id'], answer_id=answer['id'], paragraph_index=paragraph_index)
+			for paragraph_index, paragraph in enumerate(paragraphs):
+				cond_sentences = get_cond_sentences(paragraphs, question_id=question['id'], answer_id=answer['id'], paragraph_index=paragraph_index)
 				all_cond_sentences.append(cond_sentences)
 
 def main():
