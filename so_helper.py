aae9430699d7f7a78cff4d3c95c3c6a78e1bbc62
Sarah Nadi <nadi@ualberta.ca>
2018-10-05 10:36:40 -0600

    Remove unused xml processing function

--- so_helper.py
+++ so_helper.py
@@ -8,34 +8,3 @@ def get_paragraphs(post_content):
         paragraphs.append(paragraph.get_text())
 
     return paragraphs
-
-def get_all_posts_from_xml(filename):
-	for event, elem in etree.iterparse(filename, events=('start', 'end')):
-        # if counter < 100:
-        #     counter += 1
-        #     continue
-
-        # if counter  >= 563:
-        #     exit()
-
-        if event == "start":
-            if elem.tag == "row":
-                if elem.attrib["PostTypeId"] == "2": # and elem.attrib["ParentId"] == THREAD_ID:
-                    if not " if" in elem.attrib["Body"]:
-                        continue 
-
-                    XML_parsed = parse_xml_line(elem)
-
-                    if XML_parsed:
-                        for item in XML_parsed:
-                            print("| {}{}".format(elem.attrib["ParentId"], item).replace("-LRB-", "(").replace("-RRB-", ")").replace(" 't", "'t").replace(" 'd", "'d").replace(" | ", " , ")[1:])
-                            # pdb.set_trace()
-
-                        if elem.attrib["ParentId"] in threads: 
-                            threads[elem.attrib["ParentId"]] += XML_parsed
-
-                        else:
-                            threads[elem.attrib["ParentId"]] = XML_parsed
-                            tasks_set.add(elem.attrib["ParentId"])
-
-                        counter += 1
