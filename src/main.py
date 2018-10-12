ba8c4bd6a4ddd12ee3fc0bdef1b96656aebee743
Sarah Nadi <nadi@ualberta.ca>
2018-10-12 15:18:06 -0600

    Print Result

--- src/main.py
+++ src/main.py
@@ -16,14 +16,29 @@ def find_cond_sentences(questions):
 
 	return all_cond_sentences
 
+def find_word_pattern_sentences(questions):
+	all_word_pattern_sentences = list()
+	for question in questions['items']:
+		for answer in question['answers']:
+			paragraphs = get_paragraphs(answer['body'])
+			for paragraph_index, paragraph in enumerate(paragraphs):
+				word_pattern_sentences = get_word_pattern_sentences(paragraph, q_id=question['question_id'], answ_id=answer['answer_id'], parag_index=paragraph_index)
+				all_word_pattern_sentences.extend(cond_sentences)
+
+	return all_word_pattern_sentences
+
 def main():
 	load_tags()
 	SITE = StackAPI('stackoverflow')
 	questions = SITE.fetch('questions', fromdate=datetime(2011,11,11), todate=datetime(2011,11,12), min=10, sort='votes', tagged='java', filter='!-*jbN-o8P3E5')
 	init_corenlp()
 	cond_sentences = find_cond_sentences(questions)
+	baseline_sentences = find_word_pattern_sentences(questions)
 	for cond_sentence in cond_sentences:
 		cond_sentence.print('|')
+
+	for baseline_sentence in baseline_sentences:
+		baseline_sentence.print("WordPatternBaseline", '|')
 
 
 if __name__ == "__main__":
