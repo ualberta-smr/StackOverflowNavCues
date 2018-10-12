f461c7ea32ae75e4731f2944508ccf5632b20fee
Sarah Nadi <nadi@ualberta.ca>
2018-10-12 15:13:49 -0600

    Refactoring for baseline

--- src/corenlp_helper.py
+++ src/corenlp_helper.py
@@ -5,6 +5,7 @@ from regex import REGEX_LIST
 from tags import load_tags, TAGS_LIST, QUALITY_WORDS, NOUN_IDENTIFIERS
 import sys
 
+#General CoreNLP Setup
 corenlp_properties={'annotators': 'pos,parse', 'outputFormat': 'json'}
 
 corenlp = None
@@ -17,82 +18,14 @@ def init_corenlp():
 	global corenlp
 	corenlp=pycorenlp_StanfordCoreNLP('http://localhost:9000')
 
-def get_cond_sentences(paragraph, q_id, answ_id, parag_index):
-	cond_sentences = list()
-	annotations = corenlp.annotate(paragraph, corenlp_properties)
-	for sent_index, sentence in enumerate(annotations['sentences']):
-		sentence_text = get_sentence_text(sentence)
-		if " if" in sentence_text.lower():
-			#initialize conditional sentence with basic info
-			cond_sentence = ConditionalSentence(sentence=sentence_text, question_id=q_id, answer_id=answ_id, sentence_pos=sent_index, paragraph_index=parag_index)
 
-			#get information about condition, nfrs etc
-			condition = get_condition_from_sentence(sentence)
-			cond_sentence.set_condition(condition)
-			cond_sentence.set_nfreqs(get_non_func(condition))
-			nouns_in_cond = list(set(get_nouns(sentence, condition) + get_regex_nouns(sentence_text)))
-			cond_sentence.set_nouns(nouns_in_cond)
-			tags_in_cond = get_tags(nouns_in_cond)
-
-			#Our criteria for a "useful" conditional sentence is that it contains one of the SO tags in its condition
-			if (len(tags_in_cond) != 0 ):
-				cond_sentence.set_conditional()
-				cond_sentence.set_tags(tags_in_cond)
-
-			cond_sentences.append(cond_sentence)
-
-	return cond_sentences
-
-def get_regex_nouns(sentence):
-	result = []
-	for pattern in REGEX_LIST:
-		for word in sentence.split():
-			if re.match(pattern, word):
-				result.append(word)
-
-	return result
-
-def get_tags(word_list):
-	res = []
-	for word in word_list:
-		if word.lower() in TAGS_LIST:
-			res.append(word.lower())
-
-	return res
-
-
-def get_non_func(string):
-	res = []
-
-	if string:
-		for word in re.sub("[^\w]", " ",  str(string)).split():
-			if word.lower() in QUALITY_WORDS:
-				res.append(word)
-	
-	return res
-
+#General helper functions to process corenlp annotation results
 def get_sentence_text(sentence):
 	res = ""
 	for word in sentence['tokens']:
 		res += word['word'] + " "
 
 	return res
-
-
-def get_condition_from_sentence(sentence):
-	parse_res = sentence['parse']
-	split = parse_res.split("\n")
-	condition = get_tree_from_parse_items(split)
-
-	final = ""
-	for item in condition:
-			if item:
-				final += item + " "
-
-	if (len(final) == 0):
-		return None
-	else:
-		return final
 
 
 def get_index(snippet):
@@ -124,16 +57,6 @@ def get_tree_from_parse_items(items):
 
 	return res
 
-def get_nouns(sentence, condition):
-	res = []
-	words = set(re.sub("[^\w]", " ",  str(condition)).split())
-
-	for token in sentence["tokens"]:
-		if token["originalText"].lower() in words and token["pos"] in NOUN_IDENTIFIERS:
-			res.append(token["originalText"])
-
-	return res
-
 def get_word(item):
 	res = ""
 	for char in item[::-1]:
@@ -147,3 +70,110 @@ def get_word(item):
 			res = char + res
 
 	return res
+
+## Conditional Sentences
+def get_cond_sentences(paragraph, q_id, answ_id, parag_index):
+	cond_sentences = list()
+	annotations = corenlp.annotate(paragraph, corenlp_properties)
+	for sent_index, sentence in enumerate(annotations['sentences']):
+		sentence_text = get_sentence_text(sentence)
+		if " if" in sentence_text.lower():
+			#initialize conditional sentence with basic info
+			cond_sentence = ConditionalSentence(sentence=sentence_text, question_id=q_id, answer_id=answ_id, sentence_pos=sent_index, paragraph_index=parag_index)
+
+			#get information about condition, nfrs etc
+			condition = get_condition_from_sentence(sentence)
+			cond_sentence.set_condition(condition)
+			cond_sentence.set_nfreqs(get_non_func(condition))
+			nouns_in_cond = list(set(get_nouns(sentence, condition) + get_regex_nouns(sentence_text)))
+			cond_sentence.set_nouns(nouns_in_cond)
+			tags_in_cond = get_tags(nouns_in_cond)
+
+			#Our criteria for a "useful" conditional sentence is that it contains one of the SO tags in its condition
+			if (len(tags_in_cond) != 0 ):
+				cond_sentence.set_conditional()
+				cond_sentence.set_tags(tags_in_cond)
+
+			cond_sentences.append(cond_sentence)
+
+	return cond_sentences
+
+
+def get_condition_from_sentence(sentence):
+	parse_res = sentence['parse']
+	split = parse_res.split("\n")
+	condition = get_tree_from_parse_items(split)
+
+	final = ""
+	for item in condition:
+			if item:
+				final += item + " "
+
+	if (len(final) == 0):
+		return None
+	else:
+		return final
+
+def get_non_func(string):
+	res = []
+
+	if string:
+		for word in re.sub("[^\w]", " ",  str(string)).split():
+			if word.lower() in QUALITY_WORDS:
+				res.append(word)
+	
+	return res
+
+def get_regex_nouns(sentence):
+	result = []
+	for pattern in REGEX_LIST:
+		for word in sentence.split():
+			if re.match(pattern, word):
+				result.append(word)
+
+	return result
+
+def get_tags(word_list):
+	res = []
+	for word in word_list:
+		if word.lower() in TAGS_LIST:
+			res.append(word.lower())
+
+	return res
+
+
+def get_nouns(sentence, condition):
+	res = []
+	words = set(re.sub("[^\w]", " ",  str(condition)).split())
+
+	for token in sentence["tokens"]:
+		if token["originalText"].lower() in words and token["pos"] in NOUN_IDENTIFIERS:
+			res.append(token["originalText"])
+
+	return res
+
+
+## Baseline 1: Word Patterns
+def check_sentence(sentence, patterns):
+    sentence_text = get_sentence(sentence)
+    found_match = False
+
+    for pattern in patterns:
+        all_found = True
+        for item in pattern:
+            if item == "CW":
+                continue
+            if not item in sentence:
+                all_found = False
+                break
+
+        if all_found:
+            return True
+
+def get_word_pattern_sentences(paragraph, q_id, answ_id, parag_index):
+	word_pattern_sentences = list()
+	annotations = corenlp.annotate(paragraph, corenlp_properties)
+	for sent_index, sentence in enumerate(annotations['sentences']):
+		if check_word_pattern(sentence):
+			so_sentence = SOSentence(sentence=sentence_text, question_id=q_id, answer_id=answ_id, sentence_pos=sent_index, paragraph_index=parag_index)
+			word_pattern_sentences.append(so_sentence)
