ba3d17d0aeb7a964d621cdec5137fdd2c4846e34
Sarah Nadi <nadi@ualberta.ca>
2018-10-12 16:24:18 -0600

    Optimize the computation by not looping separately for each technique

--- src/main.py
+++ src/main.py
@@ -5,16 +5,23 @@ from corenlp_helper import *
 from ConditionalSentence import ConditionalSentence
 from tags import load_tags
 
-def find_cond_sentences(questions):
-	all_cond_sentences = list()
+
+def find_interesting_sentences(questions, patterns):
+	patterns = read_patterns_file()
+	all_interesting_sentences = list()
 	for question in questions['items']:
 		for answer in question['answers']:
 			paragraphs = get_paragraphs(answer['body'])
 			for paragraph_index, paragraph in enumerate(paragraphs):
-				cond_sentences = get_cond_sentences(paragraph, q_id=question['question_id'], answ_id=answer['answer_id'], parag_index=paragraph_index)
-				all_cond_sentences.extend(cond_sentences)
 
-	return all_cond_sentences
+				#get conditional sentences (our technique and also includes just sentences with "if")
+				cond_sentences = get_cond_sentences(paragraph, q_id=question['question_id'], answ_id=answer['answer_id'], parag_index=paragraph_index)
+				all_interesting_sentences.extend(cond_sentences)
+
+				#get baseline 1 sentences based on Martin's patterns
+				word_pattern_sentences = get_word_pattern_sentences(patterns,paragraph, q_id=question['question_id'], answ_id=answer['answer_id'], parag_index=paragraph_index)
+				all_interesting_sentences.extend(word_pattern_sentences)
+
 
 def read_patterns_file():
     patterns = []
@@ -25,30 +32,19 @@ def read_patterns_file():
 
     return patterns
 
-def find_word_pattern_sentences(questions, patterns):
-	all_word_pattern_sentences = list()
-	for question in questions['items']:
-		for answer in question['answers']:
-			paragraphs = get_paragraphs(answer['body'])
-			for paragraph_index, paragraph in enumerate(paragraphs):
-				word_pattern_sentences = get_word_pattern_sentences(patterns,paragraph, q_id=question['question_id'], answ_id=answer['answer_id'], parag_index=paragraph_index)
-				all_word_pattern_sentences.extend(word_pattern_sentences)
-
-	return all_word_pattern_sentences
 
 def main():
 	load_tags()
-	patterns = read_patterns_file()
 	SITE = StackAPI('stackoverflow')
 	questions = SITE.fetch('questions', fromdate=datetime(2011,11,11), todate=datetime(2011,11,12), min=10, sort='votes', tagged='java', filter='!-*jbN-o8P3E5')
 	init_corenlp()
-	cond_sentences = find_cond_sentences(questions)
-	baseline_sentences = find_word_pattern_sentences(questions, patterns)
-	for cond_sentence in cond_sentences:
-		cond_sentence.print('|')
+	interesting_sentences = find_interesting_sentences(questions)
 
-	for baseline_sentence in baseline_sentences:
-		baseline_sentence.print("WordPatternBaseline", '|')
+	for interesting_sentence in interesting_sentences
+		if isinstance(interesting_sentence, ConditionalSentence):
+			cond_sentence.print('|')
+		else:
+			baseline_sentence.print("WordPatternBaseline", '|')
 
 
 if __name__ == "__main__":
