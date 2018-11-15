5d67380e9ad09aeca83652e10a57806adbe9bb0d
Sarah Nadi <nadi@ualberta.ca>
2018-11-15 09:47:05 -0700

    Replace all link text with LINK

--- src/so_helper.py
+++ src/so_helper.py
@@ -1,10 +1,18 @@
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
 
 def get_paragraphs(post_content):
-    paragraphs = list()
-    soup = BeautifulSoup(post_content, "lxml")
+	paragraphs = list()
+	
+	only_p_tags = SoupStrainer("p")
+	soup = BeautifulSoup(post_content, "lxml", parse_only=only_p_tags)
 
-    for paragraph in soup.find_all('p'):
-        paragraphs.append(paragraph.get_text())
 
-    return paragraphs
+	for paragraph in soup:
+		#replace all link text with LINK
+		for a in paragraph.findAll('a'):
+  			a.string = "LINK"
+
+		paragraphs.append(paragraph.get_text())
+
+	return paragraphs
