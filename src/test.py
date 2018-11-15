2a8c28770b806eaf9afa6fc6070aba66a9a3e91f
Sarah Nadi <nadi@ualberta.ca>
2018-11-15 11:19:21 -0700

    Add basic ner example

---
+++ src/test.py
@@ -0,0 +1,36 @@
+from ConditionalSentence import ConditionalSentence
+from SOSentence import SOSentence
+from pycorenlp import StanfordCoreNLP as pycorenlp_StanfordCoreNLP
+import re
+from regex import REGEX_LIST
+from tags import load_tags, TAGS_LIST, QUALITY_WORDS, NOUN_IDENTIFIERS
+import sys
+
+corenlp_properties={'annotators': 'pos,parse,tokenize,ssplit,lemma,tokensregex', 'outputFormat': 'json', 'tokensregex.rules':'basic_ner.rules'}
+
+def set_core_nlp_properties(properties):
+	global corenlp_properties
+	corenlp_properties=properties
+
+def init_corenlp():
+	global corenlp
+	corenlp=pycorenlp_StanfordCoreNLP('http://localhost:9000')
+
+def get_sentence_text(sentence):
+	res = ""
+	for word in sentence['tokens']:
+		res += word['word'] + " "
+
+	return res
+
+def main():
+	init_corenlp()
+	annotations = corenlp.annotate("She has worked at Miller Corp. for 5 years. There will be a big announcement by Apple Inc today at 5:00pm. He works for apple inc in cupertino.", corenlp_properties)
+
+	for sent_index, sentence in enumerate(annotations['sentences']):
+			print("========")
+			print(sentence)
+			sentence_text = get_sentence_text(sentence)
+
+if __name__ == "__main__":
+	main()
