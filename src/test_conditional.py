bc30f2757e1abd0952ff1ef27f6b625c151b33a0
Sarah Nadi <nadi@ualberta.ca>
2019-02-06 10:30:50 -0700

    Remove trial code

--- src/test_conditional.py
+++ src/test_conditional.py
@@ -50,22 +50,6 @@ def read_question_ids():
 
 
 def main():
-	try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print 'test.py -i <inputfile> -o <outputfile>'
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print 'test.py -i <inputfile> -o <outputfile>'
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-   print 'Input file is "', inputfile
-   print 'Output file is "', outputfile
-   
 	load_tags()
 	SITE = StackAPI('stackoverflow')
 	question_ids = read_question_ids();
